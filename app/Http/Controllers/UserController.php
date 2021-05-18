62f0100d289bb318731b3a6249072d6c47833c98
Victor Gonzalez <victor.gzz.flores@hotmail.com>
2021-05-18 00:08:03 -0500

    remover limite de usuarios en peticion api de usuarios

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -12,7 +12,7 @@ class UserController extends Controller
      */
     public function index()
     {
-        $users = User::with('domicilio')->limit(10)->get();
+        $users = User::with('domicilio')->get();
         return response()->json($users);
     }
 }
